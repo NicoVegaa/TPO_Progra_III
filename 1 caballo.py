d17ff6d3d96ca9b084f9715962f058b1f8f9e318
franco <francooro322@gmail.com>
2024-10-28 01:58:13 -0300

    se agrego posiciones favorables

--- 1 caballo.py
+++ 1 caballo.py
@@ -15,6 +15,7 @@ def print_board(board):
 
 # lista para guardar las posiciones que intenta el algoritmo, para luego visualizarlo en la pantalla
 attempted_positions = []
+posiciones_favorables = []
 
 def solve_knights_tour(board, x, y, move_count):
     global total_moves
